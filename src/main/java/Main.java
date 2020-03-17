1c3f7602d9c21174ec75120100bfd315eeeb36dc
hollowsnow <uhollowu93@gmail.com>
2020-03-17 19:02:35 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import somepackage.Other;
+import somepackage.Some;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,21 @@
+import somepackage.Other;
+import somepackage.Some;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Other.saySomthing();
+
+        Some some = new Some("Как это делать?");
+        System.out.println(some.getSecret());
+        System.out.println(some.getДаТакТожеМожно());
+        System.out.println(some.getNoMoreSecrets());
+        some.setNoMoreSecrets(1234);
+        System.out.println(some.getNoMoreSecrets());
+        some.setNoMoreSecrets(213412);
+        some.getSecret(true);
+        System.out.println(some.getSecret());
+        System.out.println(some.toString());
+        System.out.println(some.count());
+    }
+}
